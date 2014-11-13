adb8c4476d158616160d69a71e53c83de4d091d1
Dane Bertram <dbertram@users.noreply.github.com>
2014-11-13 13:35:37 -0500

    use .format for string substitution

--- secretsanta.py
+++ secretsanta.py
@@ -20,7 +20,7 @@ if len(argv) < 2:
 	usage_error = True
 elif len(argv) < 3:
 	# default to the current directory if none provided
-	print "Warning: No output directory provided. Using \"%s\"\n" % dir
+	print "Warning: No output directory provided. Using \"{0}\"\n".format(dir)
 	output_dir = dir
 elif len(argv) == 3:
 	# make sure the output directory exists
@@ -43,11 +43,11 @@ if not usage_error:
 		input_file = realpath(argv[1])
 
 if usage_error:
-	print "usage: python %s input_list.txt output_dir" % script
+	print "usage: python {0} input_list.txt output_dir".format(script)
 	exit()
 
 # if we got here, everything is niffy-spiffy so far
-print "input_file = \"%s\"\noutput_dir = \"%s\"" % (input_file, output_dir)
+print "input_file = \"{0}\"\noutput_dir = \"{1}\"".format(input_file, output_dir)
 
 lines =  map(string.strip, file(input_file, "r").readlines())
 names = []
@@ -55,7 +55,7 @@ for entry in lines:
 	names.append(string.split(entry, "\t"))
 print "\nInput list:"
 for entry in names:
-	print "\t%s (%s)" % (entry[0], entry[1])
+	print "\t{0} ({1})".format(entry[0], entry[1])
 
 loop_guard = 0
 valid_order = False
@@ -74,7 +74,7 @@ while loop_guard < 10 and not valid_order:
 		
 		if not_allowed.has_key(current[0]):
 			if not_allowed[current[0]] == next[0]:
-				print "MATCH NOT ALLOWED: %s => %s" % (current[0], next[0])
+				print "MATCH NOT ALLOWED: {0} => {1}".format(current[0], next[0])
 				valid_order = False
 				break
 		pairs.append((current, next))
@@ -83,7 +83,7 @@ while loop_guard < 10 and not valid_order:
 	if valid_order:
 		if not_allowed.has_key(current[0]):
 			if not_allowed[current[0]] == first[0]:
-				print "MATCH NOT ALLOWED: %s => %s" % (current[0], first[0])
+				print "MATCH NOT ALLOWED: {0} => {1}".format(current[0], first[0])
 				valid_order = False
 		pairs.append((current, first))
 	
@@ -92,14 +92,14 @@ while loop_guard < 10 and not valid_order:
 		print "VALID ORDER!"
 
 if not valid_order:
-	print "\nERROR: The list was shuffled %d times without finding a valid ordering.\nPlease double-check that a valid ordering is possible given the following constraints:" % loop_guard
+	print "\nERROR: The list was shuffled {0} times without finding a valid ordering.\nPlease double-check that a valid ordering is possible given the following constraints:".format(loop_guard)
 	for p1 in not_allowed:
-		print "\t%s => %s NOT ALLOWED" % (p1, not_allowed[p1])
+		print "\t{0} => {1} NOT ALLOWED".format(p1, not_allowed[p1])
 	exit()
 
 if raw_input("\nDisplay matches (Y/N)? ").lower() == "y":
 	for p1, p2 in pairs:
-		print "\t%s => %s " % (p1[0], p2[0])
+		print "\t{0} => {1} ".format(p1[0], p2[0])
 
 if raw_input("\nWrite matches to disk (will overwrite existing files) (Y/N)? ").lower() == "y":
 	for p1, p2 in pairs:
@@ -135,8 +135,8 @@ if sendmail.lower() == "y":
 			"'" if to_name.endswith("s") else "'s"
 		)
 	
-		headers = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (from_address, to_address, subject)
-		message = headers + "Hey %s,\n\nYour Secret Santa is: %s!\n\nShhh...don't tell anyone! ;)\n\n-Santa's little helper\n\nP.S. This was sent by one of Santa's elves (an automated program). So please don't reply to this email...cuz then that elf will know who your secret santa is too...which would make that elf very, very sad and they would cry little elf-tears. So yeah...ix-nay on the eply-ray.\n\nOh, and if you should happen to lose this email or forget who your secret santa is you can email me (real Dane, not elf-Dane) and I can send you a file with your match without me having to know who you have. And yes, this is overly complicated. YAY CHRISTMAS!" % (to_name, match_name)
+		headers = "From: {0}\r\nTo: {1}\r\nSubject: {2}\r\n\r\n".format(from_address, to_address, subject)
+		message = headers + "Hey {0},\n\nYour Secret Santa is: {1}!\n\nShhh...don't tell anyone! ;)\n\n-Santa's little helper\n\nP.S. This was sent by one of Santa's elves (an automated program). So please don't reply to this email...cuz then that elf will know who your secret santa is too...which would make that elf very, very sad and they would cry little elf-tears. So yeah...ix-nay on the eply-ray.\n\nOh, and if you should happen to lose this email or forget who your secret santa is you can email me (real Dane, not elf-Dane) and I can send you a file with your match without me having to know who you have. And yes, this is overly complicated. YAY CHRISTMAS!".format(to_name, match_name)
 	
 		if debug.lower() == "y":
 			print "\n" + message
